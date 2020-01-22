b8fb6ac1a5f1f5db828f0f82b5fa96cc579c1597
Nourelislam <41902596+nourelislam@users.noreply.github.com>
2020-01-22 15:38:03 +0200

    Update code.sh

--- code.sh
+++ code.sh
@@ -3,8 +3,14 @@ bowtie2 -x Bowtie2_index/whole_genome.fa -1 ERR174341_1.fastq -2 ERR174341_2.fas
 
 ##Bowtie2 local alignment ##
 bowtie2 --local -x Bowtie2_index/whole_genome.fa -1 ERR174341_1.fastq -2 ERR174341_2.fastq -S ERR174341_local.sam
-
+samtools view -hbo ERR174341_local.bam ERR174341_local.sam
+samtools sort ERR174341_local.bam -o ERR174341_local.sorted.bam
+samtools index -b ERR174341_local.sorted.bam
 
 #GRIDSS
 bash gridss.sh ERR174341.sorted.bam -j gridss-2.7.3-gridss-jar-with-dependencies.jar --output ERR174341.vcf -r BWA_idx/GRCh38.p13.genome.fa -a assembly/
 #Error cannot allocate memory
+
+##Manta
+~/anaconda3/envs/ngs/share/manta-1.6.0-0/bin/configManta.py --bam ERR174341_local.sorted.bam --referenceFasta BWA_idx/GRCh38.p13.genome.fa --runDir data/
+#Error Reference genome mismatch: Normal BAM/CRAM file is missing a chromosome found in the reference fasta file: 'ML143366.1'
