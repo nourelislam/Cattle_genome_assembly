276de624dbce02715ac70cf871c4020dba199d42
Nourelislam <41902596+nourelislam@users.noreply.github.com>
2020-01-22 13:14:56 +0200

    Create code.sh

---
+++ code.sh
@@ -0,0 +1,4 @@
+
+#GRIDSS
+bash gridss.sh ERR174341.sorted.bam -j gridss-2.7.3-gridss-jar-with-dependencies.jar --output ERR174341.vcf -r BWA_idx/GRCh38.p13.genome.fa -a assembly/
+#Error cannot allocate memory
